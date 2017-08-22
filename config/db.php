dbe18cc4934b2c62542f55fae7bb1f8b8ec07d11
m-alfan <muhamad.alfan01@gmail.com>
2017-08-22 18:28:13 +0700

    setup app-rest

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=alfan',
+    'dsn' => 'mysql:host=localhost;dbname=restApi',
     'username' => 'alfan',
     'password' => 'alfan',
     'charset' => 'utf8',
--- config/params.php
+++ config/params.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => 'muhamad.alfan01@gmail.com',
+    'supportEmail' => 'noreply.resetaccess@gmail.com',
+    'name' => 'Rest API',
+    'description' => 'Rest API with Yii2',
+    'version' => 'Version 0.0.1',
 ];
